0ee6ee2b02b631aee163093ca0a5c2f102b1aa9a
Fahrek <andresgqjob@gmail.com>
2022-07-14 19:45:35 +0200

    Diseño secundario: rediseño paleta de colores del fondo de pantalla inicial, pequeña animación introductoria y paso automático hacía pantalla de login

--- app/src/main/java/com/dev/mrvazguen/indexproductorum/ui/MainActivity.java
+++ app/src/main/java/com/dev/mrvazguen/indexproductorum/ui/MainActivity.java
@@ -1,47 +1,46 @@
 package com.dev.mrvazguen.indexproductorum.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.dev.mrvazguen.indexproductorum.R;
-import com.dev.mrvazguen.indexproductorum.data.model.Articulo;
-import com.dev.mrvazguen.indexproductorum.data.model.User;
-import com.dev.mrvazguen.indexproductorum.data.repository.firestore.manager.ArticuloManagerDB;
-import com.dev.mrvazguen.indexproductorum.data.repository.firestore.manager.UserManagerDB;
-import com.dev.mrvazguen.indexproductorum.data.repository.iFirestoreNotification;
-import com.dev.mrvazguen.indexproductorum.data.repository.iTaskNotification;
-import com.dev.mrvazguen.indexproductorum.databinding.ActivityMainBinding;
-import com.dev.mrvazguen.indexproductorum.ui.fragment.home.MainFragment;
-import com.dev.mrvazguen.indexproductorum.utils.GlobarArgs;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.dev.mrvazguen.indexproductorum.ui.fragment.autentification.LoginFragment;
 
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final int SPLASH_TIME_OUT = 3300;
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    Button textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("Activity","Main Activity");
+        Log.e("Activity", "Main Activity");
+
+        topAnim    = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        imageView  = findViewById(R.id.imageView);
+        textView   = findViewById(R.id.btnEntrar);
+
+        imageView.setAnimation(topAnim);
+        textView.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, LoginFragment.class));
+            finish();
+        }, SPLASH_TIME_OUT);
 
         //TODO: Comprobar si no existe el database crear nuevo y anadir los datos de usuarios
-
-
-
-
     }
-
-
-
-
 }
